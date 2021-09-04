3f603c4b8674c7ac4050b9de59d4129809ebfbbf
Renner Borges <rennerferreira23@gmail.com>
2021-09-04 15:54:29 -0300

    feat: header sidebar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+/* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
+import './App.css';
 
-export default ()=> {
-  return (
-    <div>Olá mundo</div>
-  )
+import {
+	DonutLarge,
+	Chat,
+	MoreVert,
+} from '@material-ui/icons';
+
+export default () => {
+	return (
+		<section className="app-window">
+
+			<section className="sidebar">
+				<header>
+					<img className="header--avatar" src="https://www.w3schools.com/howto/img_avatar2.png" alt="" />
+					<div className="header-buttons">
+						<div className="header--btn">
+							<DonutLarge style={{ color: '#919191' }} />
+						</div>
+						<div className="header--btn">
+							<Chat style={{ color: '#919191' }} />
+						</div>
+						<div className="header--btn">
+							<MoreVert style={{ color: '#919191' }} />
+						</div>
+					</div>
+				</header>
+				<section className="search"></section>
+				<section className="chatlist"></section>
+			</section>
+
+			<section className="contentarea">
+
+			</section>
+		</section>
+	)
 }

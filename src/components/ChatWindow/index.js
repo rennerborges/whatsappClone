df9881ef5ab99862c56f76958ff1c8c2d4599fdc
Renner Borges <rennerferreira23@gmail.com>
2021-09-07 11:02:43 -0300

    feat: background do body

--- src/components/ChatWindow/index.js
+++ src/components/ChatWindow/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './index.css';
 
 import {
-	Search,
+    Search,
     AttachFile,
     MoreVert,
 } from '@material-ui/icons';
@@ -11,27 +11,33 @@ import {
 export default () => {
     return (
         <article className="chatWindow">
+
             <header className="chatWindow--header">
                 <div className="chatWindow--headerinfo">
-                    <img className="chatWindow--avatar" src="https://www.w3schools.com/howto/img_avatar2.png" alt=""/>
+                    <img className="chatWindow--avatar" src="https://www.w3schools.com/howto/img_avatar2.png" alt="" />
                     <div className="chatWindow--name">Renner Borges</div>
                 </div>
 
                 <div className="chatWindow--headerbuttons">
 
                     <div className="chatWindow--btn">
-                        <Search style={{color: '#919191'}}/>
+                        <Search style={{ color: '#919191' }} />
                     </div>
                     <div className="chatWindow--btn">
-                        <AttachFile style={{color: '#919191'}}/>
+                        <AttachFile style={{ color: '#919191' }} />
                     </div>
                     <div className="chatWindow--btn">
-                        <MoreVert style={{color: '#919191'}}/>
+                        <MoreVert style={{ color: '#919191' }} />
                     </div>
                 </div>
             </header>
-            <div className="chatWindow--body"></div>
+
+            <div className="chatWindow--body">
+
+            </div>
+
             <footer className="chatWindow--footer"></footer>
+
         </article>
     )
 }
